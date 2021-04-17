567520aaaabbc2325eaa3551f19ef170c45bd35c
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-18 00:41:45 +0900

    rust ss

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use std::io::{stdout, BufWriter};
 fn main() {
     // drawing_function()
     // ConstantValue_func();
-    MutableValue_func()
+    // funny_func();
+    // mutable_value_func()
+    diff_with_mut_let();
 }
 
 fn drawing_function() {
@@ -25,7 +27,36 @@ fn constant_value_func() {
     // error message let would be use for constant value
     // But, when you choose to use variation Mutable variation then,
 }
+fn funny_func() {
+    let x = 5;
+    println!("The value of x is {}", x);
+    // x = 6; Error
+
+    let x = x + 1;
+
+    let x = x + 2;
+    println!("The value of x is {}", x);
+
+    // The value of x is 5
+    // The value of x is 8
+}
+
 fn mutable_value_func() {
     let mut x = 1;
-    println!("The value of x is {}", x)
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value change to {}", x);
+
+    const MAX_POINT: u32 = 100_000;
+    println!("const practice {}", MAX_POINT);
+}
+fn diff_with_mut_let() {
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("{}", spaces)
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+    // Error =>
 }
