35cc74aa5e4a012049710751a4a5725c5fd657d8
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-17 23:56:51 +0900

    rust ss

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+// Every Function name with Snake case.
 fn main() {
-    drawing_function()
+    // drawing_function()
+    // ConstantValue_func();
+    MutableValue_func()
 }
 
 fn drawing_function() {
@@ -13,10 +16,16 @@ fn drawing_function() {
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
 }
-fn guessing_func() {
+fn constant_value_func() {
     let x = 5;
     println!("The value of x is {}", x);
+
     // x = 6;
     // println!("The value of x is {}",x);
     // error message let would be use for constant value
+    // But, when you choose to use variation Mutable variation then,
+}
+fn mutable_value_func() {
+    let mut x = 1;
+    println!("The value of x is {}", x)
 }
