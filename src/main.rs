5ba5484b3b95f2e1e1fb1e15150a9eed371e036a
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-17 23:51:32 +0900

    rust ss

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
+    drawing_function()
+}
+
+fn drawing_function() {
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+fn guessing_func() {
+    let x = 5;
+    println!("The value of x is {}", x);
+    // x = 6;
+    // println!("The value of x is {}",x);
+    // error message let would be use for constant value
 }
