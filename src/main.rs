15761c00a7271ad60030885145afec43d413c741
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-25 23:45:15 +0900

    Rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ferris_says::say;
+use std::io;
 use std::io::{stdout, BufWriter};
 
 // Every Function name with Snake case.
@@ -7,7 +8,9 @@ fn main() {
     // ConstantValue_func();
     // funny_func();
     // mutable_value_func()
-    diff_with_mut_let();
+    // diff_with_mut_let();
+
+    processingGuess();
 }
 
 fn drawing_function() {
@@ -54,9 +57,23 @@ fn diff_with_mut_let() {
     let spaces = "   ";
     let spaces = spaces.len();
 
-    println!("{}", spaces) 
+    println!("{}", spaces)
 
     // let mut spaces = "   ";
     // spaces = spaces.len();
     // Error =>
+}
+
+fn processingGuess() {
+    println!("Guess the number !");
+    println!("Please input your guess");
+
+    // Storing Values with Variables
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to readline");
+
+    println!("Your Number : {}", guess);
 }
